7c54c6bf7b8042e33caff03992a8f9c5af1bbdc0
Raya Naouar <raia.naouar@gmail.com>
2021-07-11 00:40:37 +0100

    [Fix] sign in component layout & add form controls

--- src/app/main-feature/authentication/pages/sign-in/sign-in.component.ts
+++ src/app/main-feature/authentication/pages/sign-in/sign-in.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.sass']
+  styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+
+
+  email: FormControl
+  loginForm: FormGroup
+  password: FormControl
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm()
+  }
+
+
+  initForm() {
+    this.email = new FormControl('', { validators: [Validators.required, Validators.email] })
+    this.password = new FormControl('', { validators: Validators.required })
+
+    this.loginForm = new FormGroup({
+      email: this.email,
+      password: this.password
+    })
+
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+    }
   }
 
 }
