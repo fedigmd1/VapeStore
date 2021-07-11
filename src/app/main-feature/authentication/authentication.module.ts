ca473808c74e8643f9eddcbb03a5bf5c50ab4dbe
Raya Naouar <raia.naouar@gmail.com>
2021-07-11 16:07:47 +0100

    [Fix] add provider

--- src/app/main-feature/authentication/authentication.module.ts
+++ src/app/main-feature/authentication/authentication.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AuthService } from './service/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
 import { AuthenticationMainComponent } from './authentication-main/authentication-main.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -25,6 +24,7 @@ import { MatCardModule } from '@angular/material/card';
     ReactiveFormsModule,
     AuthenticationRoutingModule
   ],
+  providers: [AuthService]
   
 })
 export class AuthenticationModule { }
