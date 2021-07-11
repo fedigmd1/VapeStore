a58392a7f5e488bda664b8c8cfcd86295da8f1ea
Fedi Gammoudi <gammoudifedi@gmail.com>
2021-07-11 16:29:46 +0100

    [Fix] product routing store

--- src/app/main-feature/shop/widgets/categories/categories.component.ts
+++ src/app/main-feature/shop/widgets/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.sass']
 })
 export class CategoriesComponent implements OnInit {
+
+  @Input() id: number
 
   constructor() { }
 
