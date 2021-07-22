52e2c8cb6f8457784e99f54aad1403d8a0eea1e7
Raya Naouar <raia.naouar@gmail.com>
2021-07-22 19:28:03 +0100

    [Fix] add user profile

---
+++ src/app/main-feature/profile/profile-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainProfileComponent } from './main-profile/main-profile.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'profile' },
+  {
+    path: '', component: MainProfileComponent , children: [
+      { path: 'profile', component: ProfileComponent }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class ProfileRoutingModule { }
---
+++ src/app/main-feature/profile/profile.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './profile/profile.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MainProfileComponent } from './main-profile/main-profile.component';
+import { ProfileRoutingModule } from './profile-routing.module';
+
+
+
+@NgModule({
+  declarations: [ProfileComponent, MainProfileComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    ProfileRoutingModule
+    
+  ]
+})
+export class ProfileModule { }
