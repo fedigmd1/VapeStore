69620d28394cbb6a27e018ab015cc2560e746e37
Raya <raia.naouar@gmail.com>
2021-07-28 19:33:43 +0100

    [Refactor] change update profile screen and implementation

--- src/app/main-feature/profile/profile/profile.component.ts
+++ src/app/main-feature/profile/profile/profile.component.ts
@@ -5,6 +5,8 @@ import { User } from 'src/app/core/models/auth';
 import { ModalService } from 'src/app/shared/services/modal.service';
 import { SessionService } from 'src/app/shared/services/session.service';
 import { CustomValidators } from 'src/app/shared/validators/confirmPassword.validator';
+import { ProfileService } from '../services/profile.service';
+import { AuthService } from '../../authentication/service/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -15,46 +17,61 @@ export class ProfileComponent implements OnInit {
 
   @ViewChild('inputFile') inputFile: ElementRef;
 
+  updatedUser: any;
   userPicture: null
   userDetails: User
-  email: FormControl
   phone: FormControl
   address: FormControl
-  password: FormControl
   lastName: FormControl
   firstName: FormControl
-  showUpdateForm = false
+  showEditAdress = false
+  oldPassword: FormControl
+  password: FormControl
+  showEditUserName = false
+  showEditPassword = false
   validatepasswords = false
+  showEditPhoneNumber = false
   updateProfileForm: FormGroup
   confirmPassword: FormControl
+  updatePasswordForm: FormGroup
 
 
   constructor(
     private modalService: ModalService,
     private sessionService: SessionService,
+    private profileSession: ProfileService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
     this.getUserDetails()
+    
+    this.initForm()
   }
 
   initForm() {
-    this.phone = new FormControl(this.userDetails.phone, { validators: Validators.required })
-    this.address = new FormControl(this.userDetails.address, { validators: Validators.required })
-    this.password = new FormControl(this.userDetails.password, { validators: Validators.required })
-    this.lastName = new FormControl(this.userDetails.last_name, { validators: Validators.required })
-    this.firstName = new FormControl(this.userDetails.first_name, { validators: Validators.required })
-    this.confirmPassword = new FormControl(this.userDetails.password, { validators: Validators.required })
-    this.email = new FormControl({ value: this.userDetails.email, disabled: true }, { validators: [Validators.required, Validators.email] })
+    this.phone = new FormControl(this.userDetails.phone)
+    this.address = new FormControl(this.userDetails.adress)
+    this.lastName = new FormControl(this.userDetails.last_name)
+    this.firstName = new FormControl(this.userDetails.first_name)    
+
+    this.password = new FormControl({validators: Validators.required})
+    this.oldPassword = new FormControl({validators: Validators.required})
+    this.confirmPassword = new FormControl({validators: Validators.required})
 
     this.updateProfileForm = new FormGroup({
-      email: this.email,
       phone: this.phone,
       address: this.address,
       lastName: this.lastName,
-      password: this.password,
       firstName: this.firstName,
+    })
+
+    this.updatePasswordForm = new FormGroup({
+
+      password: this.password,
+      oldPassword: this.oldPassword,
       confirmPassword: this.confirmPassword
+
     }, {
       validators: CustomValidators.mustMatchPwd
     })
@@ -64,29 +81,92 @@ export class ProfileComponent implements OnInit {
     this.userDetails = JSON.parse(this.sessionService.getUserDetails())
   }
 
-  showForm() {
-    this.initForm()
-    this.showUpdateForm = !this.showUpdateForm
+  showEditPasswordForm() {
+    this.showEditPassword = !this.showEditPassword
+    this.showEditUserName = false
+    this.showEditAdress = false
+    this.showEditPhoneNumber= false
+
+
+
+  }
+  showEditUsenameForm() {
+    this.showEditUserName = !this.showEditUserName
+    this.showEditPassword = false
+    this.showEditAdress = false
+    this.showEditPhoneNumber= false
+
+
+
+  }
+  showEditAdressForm() {
+    this.showEditAdress = !this.showEditAdress
+    this.showEditPassword = false
+    this.showEditUserName = false
+    this.showEditPhoneNumber= false
+  }
+  showEditPhoneNbrForm() {
+    this.showEditPhoneNumber= !this.showEditPhoneNumber
+    this.showEditAdress = false
+    this.showEditPassword = false
+    this.showEditUserName =false
   }
 
-  upadateProfile() {
-    this.updateProfileForm.errors?.passwordNotmatch ? this.validatepasswords = true : this.validatepasswords = false
-
-    if (this.updateProfileForm.valid) {
+updatePassword(){
+    this.updatePasswordForm.errors?.passwordNotmatch ? this.validatepasswords = true : this.validatepasswords = false
+    if (this.updatePasswordForm.valid) {
 
       const requestData = {
-        email: this.updateProfileForm.value.email,
-        phone: this.updateProfileForm.value.phone,
-        address: this.updateProfileForm.value.address,
-        password: this.updateProfileForm.value.password,
-        last_name: this.updateProfileForm.value.lastName,
-        first_name: this.updateProfileForm.value.firstName,
+        old_password: this.updatePasswordForm.value.oldPassword,
+        password: this.updatePasswordForm.value.password
       }
       console.log(requestData);
-
-      //TODO WS
+      this.profileSession.updateProfile(requestData).subscribe(res => {
+        this.authService.getUserInfo()
+        console.log(res);
+      })
     }
+}
+
+updateInfo() {
+  if (this.updateProfileForm.invalid) {
+    return;
   }
+  this.updateCurrentUser();
+  this.updateProfile();
+  this.showEditPhoneNumber= false
+  this.showEditAdress = false
+  this.showEditPassword = false
+  this.showEditUserName =false
+}
+
+  updateProfile() {
+      const requestData = {
+        phone: this.updatedUser?.phone,
+        adress: this.updatedUser?.adress,
+        last_name: this.updatedUser?.last_name,
+        first_name: this.updatedUser?.first_name,
+      }
+      console.log(requestData);
+      this.profileSession.updateProfile(requestData).subscribe(res => {
+        this.authService.getUserInfo()
+        console.log(res);
+        
+      })
+    
+  }
+
+
+  updateCurrentUser() {
+    this.updatedUser = {
+      ...this.userDetails,
+      phone: this.updateProfileForm.controls.phone.value,
+      adress: this.updateProfileForm.controls.address.value,
+      last_name: this.updateProfileForm.controls.lastName.value,
+      first_name: this.updateProfileForm.controls.firstName.value
+    };
+  }
+
   openFile() {
     this.inputFile.nativeElement.click();
   }
