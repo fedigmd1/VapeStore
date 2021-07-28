5d5e572234e9c9eec5094b1b9c1f91b741571427
Raya <raia.naouar@gmail.com>
2021-07-28 19:29:26 +0100

    [Fix] user model

--- src/app/core/models/auth.ts
+++ src/app/core/models/auth.ts
@@ -2,7 +2,7 @@ export interface User {
     role: string
     email: string
     phone: string
-    address: string
+    adress: string
     password: string
     last_name: string
     first_name: string
