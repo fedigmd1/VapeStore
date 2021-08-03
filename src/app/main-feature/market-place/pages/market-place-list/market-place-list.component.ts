0600174a9569005f61f8e82d0f6924e850799f7b
Raya <raia.naouar@gmail.com>
2021-08-03 10:27:36 +0100

    [Fix] show modal

--- src/app/main-feature/market-place/pages/market-place-list/market-place-list.component.ts
+++ src/app/main-feature/market-place/pages/market-place-list/market-place-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalService } from 'src/app/shared/services/modal.service';
 
 @Component({
   selector: 'app-market-place-list',
@@ -8,7 +9,9 @@ import { Router } from '@angular/router';
 })
 export class MarketPlaceListComponent implements OnInit {
   id = 6
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private modalService: ModalService) { }
 
   ngOnInit() {
   }
@@ -17,6 +20,12 @@ export class MarketPlaceListComponent implements OnInit {
     this.id = id
     let url = 'market-place/post/'+this.id +'/details'
     this.router.navigate([url])
+  }
+
+  showAddPostModal(){
+    this.modalService.AddPostModal().subscribe(res => {
+
+    });
   }
 
 
