99143d039f2ea659c7f97e9a303fd5066c8461c8
Raya <raia.naouar@gmail.com>
2021-08-06 00:02:39 +0100

    [fix] add update post modale + fix form

---
+++ src/app/main-feature/market-place/modals/update-post/update-post.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit,EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-update-post',
+  templateUrl: './update-post.component.html',
+  styleUrls: ['./update-post.component.scss']
+})
+export class UpdatePostComponent implements OnInit {
+  
+  postDetails
+  postImage
+  uploadedImage
+  title: FormControl
+  description: FormControl
+  updatePostForm: FormGroup
+  public event: EventEmitter<any> = new EventEmitter();
+
+  constructor(public bsModalRef: BsModalRef) { }
+
+  ngOnInit(): void {
+    this.initForm()
+  }
+
+  initForm() {
+    console.log(this.postDetails);
+
+    this.title = new FormControl(this.postDetails.title, { validators: Validators.required })
+    this.description = new FormControl(this.postDetails.description, { validators: Validators.required })
+
+
+    this.updatePostForm = new FormGroup({
+      title: this.title,
+      description: this.description,
+    })
+  }
+
+  uploadImage(event) {
+    this.uploadedImage = event.target.files[0]
+    const reader = new FileReader();
+    reader.readAsDataURL(this.uploadedImage);
+    reader.onload = () => {
+      this.postImage = reader.result
+      console.log(this.postImage);
+      
+    }
+  }
+
+  deleteUploadedImage() {
+    this.uploadedImage = null
+  }
+
+  hideModal() {
+    this.bsModalRef.hide();
+  }
+
+  updatePost() {
+    if (this.updatePostForm.valid) {
+      const requestData = {
+        picture: this.postImage,
+        post_id: this.postDetails._id,
+        title: this.updatePostForm.value.title,
+        description: this.updatePostForm.value.description
+      }
+      console.log(requestData);
+      this.event.emit(requestData)
+      this.hideModal()
+    }
+  }
+
+}
